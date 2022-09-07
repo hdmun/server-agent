8e275002e00dcc3a1eb436d6b0f8219c7036d737
hdmun <hdmun@kakao.com>
2022-09-07 12:39:43 +0900

    `log4net` DB 테이블 로깅 설정 추가

---
+++ Database/Tables/ServerAgentLog.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[ServerAgentLog]
+(
+	[Id] INT IDENTITY (1, 1) NOT NULL PRIMARY KEY, 
+    [Date] datetime NOT NULL,
+    [Thread] varchar(255) NOT NULL,
+    [Level] varchar(50) NOT NULL,
+    [Logger] varchar(255) NOT NULL,
+    [Message] nvarchar(4000) NOT NULL,
+    [Exception] nvarchar(2000) NULL
+)
